f4c831615dda60fc7e1c72a759a544c2009a8e34
Tzur Soffer <tzur.soffer@gmail.com>
2026-07-07 01:37:29 +0100

    Version bump

--- uploadDockerNightly.ps1
+++ uploadDockerNightly.ps1
@@ -1,6 +1,6 @@
 Get-ChildItem -Recurse -Directory -Filter "__pycache__" | Remove-Item -Recurse -Force
 docker build --no-cache -t spotify-tracker:nightly .
-# docker build -t spotify-tracker:latest .
+# docker build -t spotify-tracker:nightly .
 docker tag spotify-tracker:nightly mepro3/spotify-tracker:nightly
 Write-Host "Check that everything looks good, once done, type 'exit'"
 docker run --rm -it spotify-tracker:nightly sh
